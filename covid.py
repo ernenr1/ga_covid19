0c76f04dce35d2b44f904be44fac9f87adaeab0e
Ernie Enriquez <eenriquez315@hotmail.com>
2020-04-24 23:34:56 -0400

    Update. Changed headers.

--- covid.py
+++ covid.py
@@ -14,9 +14,9 @@ df = dfimport.loc[0:50, ['state', 'fips', 'positive', 'death']]
 
 df['text'] = "State: "+df['state'].astype(str) +"<br>"+ "Positive Cases: "+df['positive'].astype(str) +"<br>"+ "Deaths: "+df['death'].astype(str)
 
-data = [dict(type='choropleth', autocolorscale=True, locations=df['state'], z=df['positive'], locationmode='USA-states', text=df['text'], colorbar= dict(title="Postive Cases"))]
+data = [dict(type='choropleth', autocolorscale=True, locations=df['state'], z=df['positive'], locationmode='USA-states', text=df['text'], colorbar = dict(title="Postive Cases"))]
 
-layout = dict(title='COVID-19 Postive/Death Cases in the US',geo = dict(scope='usa', projection=dict(type='albers usa'), showlakes= True, lakecolor='rgb(66,165,245)',),)
+layout = dict(title='Positive COVID-19 Cases in the US', geo = dict(scope='usa', projection=dict(type='albers usa'), showlakes= True, lakecolor='rgb(66,165,245)',),)
 
 fig = dict(data=data,layout=layout)
 
