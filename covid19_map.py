0a974684715bb1544ce8f3c10c0928926a9c6ae7
Ernie Enriquez <eenriquez315@hotmail.com>
2020-04-26 21:21:56 -0400

    CSS fixes. Added news tracker. Abstracted Key.

---
+++ covid19_map.py
@@ -0,0 +1,152 @@
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+import requests
+
+"""
+Request world data
+"""
+
+url = "https://api.covid19api.com/summary"
+
+payload = {}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+world_data = response.json()
+
+df_raw1 = pd.json_normalize(world_data["Countries"])
+
+df_world=df_raw1.loc[0:246,['Country', 'CountryCode', 'TotalConfirmed', 'TotalDeaths', 'TotalRecovered']]
+
+df_world['TotalDeaths'] = df_world['TotalDeaths'].map(lambda x: "{:,}".format(x))
+df_world['PositiveCases'] = df_world['TotalConfirmed'].map(lambda x: "{:,}".format(x))
+
+
+df_world['text'] = "Country: "+df_world['CountryCode'].astype(str) +"<br>"+ "Positive Cases: "+df_world['PositiveCases'].astype(str) +"<br>"+ "Deaths: "+df_world['TotalDeaths'].astype(str)
+
+world_fig = go.Figure(data=go.Choropleth(
+    locations = df_world['Country'],
+    z = df_world['TotalConfirmed'],
+    text = df_world['text'],
+    colorscale = 'Reds',
+    locationmode = 'country names',
+))
+
+world_fig.update_layout(
+    title_text='Test',
+    geo=dict(
+        #showframe=False,
+        showcoastlines=False,
+        projection_type='equirectangular'
+    )
+)
+
+"""
+READ IN JSON data
+From COVID19 API @
+https://covidtracking.com/api/v1/states/current.json
+"""
+
+df_raw2 = pd.read_json('https://covidtracking.com/api/v1/states/current.json')
+
+#Import data clean
+df_us = df_raw2.loc[0:50, ['state', 'fips', 'positive', 'death']]
+
+df_us['death'] = df_us['death'].map(lambda x: "{:,}".format(x))
+df_us['positivecases'] = df_us['positive'].map(lambda x: "{:,}".format(x))
+
+#Create new column in dataframe to include text
+df_us['text'] = "State: "+df_us['state'].astype(str) +"<br>"+ "Positive Cases: "+df_us['positivecases'].astype(str) +"<br>"+ "Deaths: "+df_us['death'].astype(str)
+
+#Create Plotly Choropleth
+
+us_fig = go.Figure(data=go.Choropleth(
+    locations=df_us['state'], # Spatial coordinates
+    z = df_us['positive'], # Data to be color-coded
+    text=df_us['text'],
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = 'Reds',
+    colorbar_title = "Positive Cases",
+))
+
+us_fig.update_layout(
+    title_text = 'Positive COVID-19 Cases in the US',
+    geo_scope='usa', # limit map scope to USA
+)
+"""
+DASH App intialization
+"""
+
+def update_news():
+    news_url = "http://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=[KEY]"
+    response = requests.request("GET", news_url, headers=headers, data = payload)
+    news_data = response.json()
+    df_news_import = pd.json_normalize(news_data["articles"])
+    df_news = df_news_import.loc[0:20, ["title", "url"]]
+    return df_news
+
+def news_html_table(max_rows=15):
+    df_news_html = update_news()
+    return html.Div(
+        [
+            html.Div(
+                html.Table(
+                    #Header
+                    [html.Tr([html.Th()])]
+                    +
+                    #body
+                    [
+                        html.Tr(
+                            [
+                                html.Td(
+                                    html.A(
+                                        df_news_html.iloc[i]["title"],
+                                        href=df_news_html.iloc[i]["url"],
+                                        target="_blank"
+                                    )
+                                )
+                            ]
+                        )
+                        for i in range(min(len(df_news_html), max_rows))
+                    ]
+                ),
+                style={"height": "750px", "overflowY": "scroll"},
+            ),
+        ],
+        style={"height": "100%"}
+    )
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+github = str('https://www.google.com/')
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div(children=[
+    html.H1(children='Hello World!'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+
+    html.Div([
+        dcc.Graph(id='g1', figure=world_fig)
+    ], className="eight columns"),
+    html.Div([
+        dcc.Graph(id='g2', figure=us_fig),
+        html.A('Code on Github', href=github)
+    ], className="eight columns"),
+
+    html.Div([
+        html.H3("COVID News"),
+        news_html_table()
+    ]),
+])
+if __name__ == '__main__':
+    app.run_server(debug=True)
